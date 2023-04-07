1b7a30829fd2001a4aeb01da4f86b8cf19606a04
cardairi <carlairiza@gmail.com>
2023-04-07 19:28:26 -0300

    adding card rendering

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,47 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { ContextGlobal } from "../Components/utils/global.context";
+import { useContext } from "react";
 
 const Card = ({ name, username, id }) => {
+  const { state, dispatch } = useContext(ContextGlobal);
+  const isFav = state.favs.some((fav) => fav.id === id);
 
-  const addFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage
-  }
+  const handleFavClick = () => {
+    if (isFav) {
+      dispatch({ type: "REMOVE_FAV", payload: { id, name, username } });
+      alert("Successfully removed");
+    } else {
+      dispatch({ type: "ADD_FAV", payload: { id, name, username } });
+      alert("Successfully added");
+    }
+  };
 
   return (
-    <div className="card">
-        {/* En cada card deberan mostrar en name - username y el id */}
+    <div
+      className="card"
+      style={{
+        background: state.theme.backgroundCard,
+        color: state.theme.color,
+      }}
+    >
+      <img src="./images/doctor.jpg" alt="" width={200} />
 
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+      <Link to={`/detail/${id}`}>
+        <p>{name}</p>
+      </Link>
+      <p>{username}</p>
+      <button className="favButton" onClick={handleFavClick}>
+        {isFav ? (
+          <>
+            <span>Remove </span>❌
+          </>
+        ) : (
+          <>
+            <span>Add </span>⭐
+          </>
+        )}
+      </button>
     </div>
   );
 };
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import Card from '../Components/Card'
+import { useContext } from 'react'
+import { ContextGlobal } from '../Components/utils/global.context'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  const {state} = useContext(ContextGlobal);
+
   return (
-    <main className="" >
+    <main style={{ background: state.theme.backgroundHome}}>
       <h1>Home</h1>
       <div className='card-grid'>
         {/* Aqui deberias renderizar las cards */}
+        {state.data.map((item) => (
+          <Card key={item.id} id={item.id} name={item.name} username={item.username}/>
+        ))}
       </div>
     </main>
   )
