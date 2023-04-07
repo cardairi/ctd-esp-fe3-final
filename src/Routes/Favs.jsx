ec7315b3b32aadc65b0fb2fec31fa9abb258b97e
cardairi <carlairiza@gmail.com>
2023-04-07 19:30:56 -0300

    adding favs with local storge logic

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import Card from "../Components/Card";
-
+import { ContextGlobal } from '../Components/utils/global.context'
+import { useContext } from 'react'
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
 
+  const { state } = useContext(ContextGlobal);
+
+  const favoriteDentists = state.favs;
+
+
   return (
-    <>
+    <div style={{ background: state.theme.backgroundHome}}>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
+      {favoriteDentists.map((favoriteDentist) => (
+    <Card
+      key={favoriteDentist.id}
+      id={favoriteDentist.id}
+      name={favoriteDentist.name}
+      username={favoriteDentist.username}
+    />
+  ))}
       </div>
-    </>
+    </div>
   );
 };
 
