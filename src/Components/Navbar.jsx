052abded32663cbb8260f1b704a1d1b9cfd3213c
cardairi <carlairiza@gmail.com>
2023-04-07 19:23:29 -0300

    add links to navbar and dark theme

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useContext } from 'react'
+import { ContextGlobal } from './utils/global.context'
+import { Link } from 'react-router-dom'
+
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
+  const {state,toggleTheme} = useContext(ContextGlobal);
+  
   return (
-    <nav>
+    <nav  style={{ background: state.theme.backgroundNavbar, color:state.theme.color }} >
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <Link to="/">Home</Link>
+      <Link to="/contacto">Contacto</Link>
+      <Link to="/favs">Favs</Link>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <button onClick={toggleTheme}>
+        Change theme</button>
     </nav>
   )
 }
--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,14 +1,91 @@
-import { createContext } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
-export const initialState = {theme: "", data: []}
+export const initialState = {
+  theme: {
+    light: {
+      backgroundHome: "#F5F5F4",
+      backgroundNavbar: "#818CF8",
+      backgroundFooter: "#818CF8",
+      backgroundCard: " #818CF8",
+      color: "#000000",
+    },
+    dark: {
+      backgroundHome: "#E2E8F0",
+      backgroundNavbar: "#020617",
+      backgroundFooter: "#020617",
+      backgroundCard: "#020617",
+      color: "#ffffff",
+    },
+  },
+  data: [],
+  favs: JSON.parse(localStorage.getItem("favs")) || [],
+};
+export const ContextGlobal = createContext(initialState);
 
-export const ContextGlobal = createContext(undefined);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_THEME":
+      const newTheme =
+        state.theme === initialState.theme.light
+          ? initialState.theme.dark
+          : initialState.theme.light;
+      return {
+        ...state,
+        theme: newTheme,
+      };
+    case "SET_DATA":
+      return {
+        ...state,
+        data: action.payload,
+      };
+    case "ADD_FAV":
+      return {
+        ...state,
+        favs: [...state.favs, action.payload],
+      };
+    case "REMOVE_FAV":
+      return {
+        ...state,
+        favs: state.favs.filter((fav) => fav.id !== action.payload.id),
+      };
+    default:
+      return state;
+  }
+};
 
 export const ContextProvider = ({ children }) => {
-  //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+  const [state, dispatch] = useReducer(reducer, {
+    ...initialState,
+    theme: initialState.theme.dark,
+  });
+
+  useEffect(() => {
+    localStorage.setItem("favs", JSON.stringify(state.favs));
+  }, [state.favs]);
+
+  console.log(state);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        dispatch({ type: "SET_DATA", payload: data });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const toggleTheme = () => {
+    dispatch({ type: "TOGGLE_THEME" });
+  };
 
   return (
-    <ContextGlobal.Provider value={{}}>
+    <ContextGlobal.Provider value={{ toggleTheme, state, dispatch }}>
       {children}
     </ContextGlobal.Provider>
   );
