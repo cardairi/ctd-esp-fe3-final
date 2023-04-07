4cbb98382e797131de8dacaf1551697d28316800
cardairi <carlairiza@gmail.com>
2023-04-07 19:09:09 -0300

    Add rutes to the principal page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 
+// importing routes
+import Contact from "./Routes/Contact";
+import Home from "./Routes/Home";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/contacto" element={<Contact />} />
+        <Route path="/favs" element={<Favs />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
