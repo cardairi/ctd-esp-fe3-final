882a98956f736e66582634192179fed7813fedcf
cardairi <carlairiza@gmail.com>
2023-04-07 19:35:57 -0300

    adding form contacts wuth validation regex

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [user,setUser] = useState({
+      name: ' ',
+      email: ' '
+  })
 
+  const [message, setMessage] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(user.name.length > 5){
+      const emailRegex = /[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'+/=?^_`{|}~-]+)@(?:[a-z0-9](?:[a-z0-9-][a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+      if(emailRegex.test(user.email)){
+        setMessage(`Gracias ${user.name}, te contactaremos cuanto antes vía mail`)
+      } else {
+        setMessage(`Por favor verifique su información nuevamente`)
+      }
+    } else {
+      setMessage(`Por favor verifique su información nuevamente`)
+    }
+    
+  }
   return (
-    <div>
-      <form>
+    <div className="state">
+      <form onSubmit={handleSubmit}>
+      <label>Nombre</label>
+        <input type="text" onChange={(e) => setUser({...user, name: e.target.value})}/>
+        <label>Email</label>
+        <input type="email" onChange={(e) => setUser({...user, email: e.target.value})}/>
+        <button>Send</button>
       </form>
+      <h1>{message}</h1>
     </div>
   );
 };
+
+
 
 export default Form;
--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Form from '../Components/Form'
+import { useContext } from 'react'
+import { ContextGlobal } from '../Components/utils/global.context'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contact = () => {
+  const {state} = useContext(ContextGlobal);
+
   return (
-    <div>
+    <div style={{ background: state.theme.backgroundCard, color:state.theme.color }}>
       <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
+      <p  style={{ textAlign: 'center', paddingBottom:'1rem'}}>Send us your questions and we will contact you</p>
       <Form/>
     </div>
   )
